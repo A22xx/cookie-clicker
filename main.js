20d74f2e2a1f3207ba3e1964b302888383aaae39
Sia <tbx00182@gmail.com>
2025-05-02 16:21:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ document.getElementById("cookieRestart").addEventListener("click", function() {
     currentNum = 0;
     document.getElementById("currentNum").textContent = currentNum;
 });
+
+let lastTap = 0;
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("touchend", function(e) {
+        const currentTime = new Date().getTime();
+        const tapLength = currentTime - lastTap;
+        if (tapLength < 300 && tapLength > 0) {
+            e.preventDefault();
+        }
+        lastTap = currentime;
+    });
+});
